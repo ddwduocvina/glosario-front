218806ce00bf2d59cececb698cb5effa81c11933
Paola Véliz <paola.veliz@gmail.com>
2024-01-23 20:18:59 -0300

    ajuste modo oscuro

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,9 +37,9 @@
       }
   
       const themeSwitcherText = document.querySelector('#bd-theme-text')
-      const activeThemeIcon = document.querySelector('.theme-icon-active')
+      //const activeThemeIcon = document.querySelector('.theme-icon-active')
       const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`)
-      const svgOfActiveBtn = btnToActive.querySelector('svg').getAttribute('href')
+      //const svgOfActiveBtn = btnToActive.querySelector('svg').getAttribute('href')
   
       document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
         element.classList.remove('active')
@@ -48,7 +48,7 @@
   
       btnToActive.classList.add('active')
       btnToActive.setAttribute('aria-pressed', 'true')
-      activeThemeIcon.setAttribute('href', svgOfActiveBtn)
+      //activeThemeIcon.setAttribute('href', svgOfActiveBtn)
       const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`
       themeSwitcher.setAttribute('aria-label', themeSwitcherLabel)
   
